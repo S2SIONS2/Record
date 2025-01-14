7e473647fcfd705f10bcf063f59b97fb9c896ae5
Sion <solee9802@gmail.com>
2025-01-14 22:19:43 +0900

    로그인 페이지 퍼블리싱

--- record/src/app/(before-login)/layout.tsx
+++ record/src/app/(before-login)/layout.tsx
@@ -1,5 +1,6 @@
 import LoginNav from "@/components/login-nav";
 import { Suspense } from "react";
+import style from './page.module.css';
 
 export default function Page({children}: Readonly<{children: React.ReactNode}>) {
     return (
@@ -7,7 +8,9 @@ export default function Page({children}: Readonly<{children: React.ReactNode}>) 
             <Suspense fallback={<div>Loading...</div>}>
                 <LoginNav />
             </Suspense>
-            {children}
+            <div className={style.container}>
+                {children}
+            </div>
         </div>
     )
 }
--- record/src/app/(before-login)/login/page.tsx
+++ record/src/app/(before-login)/login/page.tsx
@@ -1,8 +1,38 @@
+'use client';
+
+import { useState } from 'react';
+import style from './page.module.css';
+import Link from 'next/link';
 
 export default function Page() {
+    const [email, setEmail] = useState('');
+    const handleEmail = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setEmail(e.target.value);
+    }
+
+    const [pw, setPw] = useState('');
+    const handlePw = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setPw(e.target.value);
+    }
+
+    const signInBtn = () => {
+
+    }
+    
     return (
-        <div>
-            login
+        <div className={style.wrap}>
+            <h3 className={style.title}>Record</h3>
+            <div className={style.input_wrap}>
+                <p>아이디</p>
+                <input type="text" value={email} onChange={handleEmail}/>
+            </div>
+            <div className={style.input_wrap}>
+                <p>비밀번호</p>
+                <input type="password" value={pw} onChange={handlePw}/>
+            </div>
+            
+            <button type="button" onClick={signInBtn} className={style.signInBtn}>로그인</button>
+            <Link href={'/signup'} className={style.link}>회원가입하기</Link>
         </div>
     )
 }
