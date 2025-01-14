5e6648cbab0850d2f14d35b4b2a2637c58e62127
Sion <solee9802@gmail.com>
2025-01-14 22:58:28 +0900

    로그아웃 버튼 추가

--- record/src/components/main-nav.tsx
+++ record/src/components/main-nav.tsx
@@ -1,12 +1,20 @@
+'use client'
+
 import Link from "next/link";
 import style from './main-nav.module.css';
+import { useRouter } from "next/navigation";
 
 export default function MainNav() {
+    const router = useRouter();
+    const logout = () => {
+        router.push('/login');
+    }
     return (
         <nav className={style.nav}>
             <Link href={'/main'} className={style.logo}>Record</Link>
             <div className={style.navDiv}>
                 <Link href={'/main/mypage'}>마이페이지</Link>
+                <button onClick={logout} className={style.logoutBtn}>로그아웃</button>
             </div>
         </nav>
     )
